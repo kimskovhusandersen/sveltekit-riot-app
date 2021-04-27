0cb956cac647b0f9fdd262f85ea1305483067f47
Kim Skovhus Andersen <k.s.andersen@kenoxa.de>
2021-04-27 15:15:41 +0200

    add adapter

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,4 +1,5 @@
 const preprocess = require('svelte-preprocess');
+const adapter = require('@sveltejs/adapter-vercel');
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
@@ -8,6 +9,7 @@ module.exports = {
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: adapter()
 	}
 };
